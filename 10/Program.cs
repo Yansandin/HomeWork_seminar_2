e7e252bf411f5aef1eed6b5b196ed23be33ca9cf
San <yan_san_din@mail.ru>
2023-01-19 02:03:53 +0300

    new program

---
+++ 10/Program.cs
@@ -0,0 +1,9 @@
+﻿
+int number = new Random().Next(100, 1000);
+
+Console.WriteLine("Сгенерировано трехзначное число " + number);
+
+int FirstDigit = number % 100;
+int SecondDigit = FirstDigit / 10;
+
+Console.WriteLine("Вторая цифра числа " + SecondDigit);
