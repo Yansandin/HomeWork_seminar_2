8e61ea90fad2c18d8490b6316642aaabbf4177ac
San <yan_san_din@mail.ru>
2023-01-20 23:11:05 +0300

    first commit

---
+++ 15/Program.cs
@@ -0,0 +1,40 @@
+﻿//Console.Write("Введите число чтобы узнать день недели: ");
+
+//int number = Convert.ToInt32(Console.ReadLine());
+
+//if(number<6||number>1)
+//{
+   //Console.WriteLine("Будний день");
+//}
+
+//if(number==6||number==7)
+//{
+ //   Console.WriteLine("Выходной день");
+//}
+
+Console.Write("Введите число чтобы узнать день недели: ");
+
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if(number==1)
+Console.WriteLine("Понедельник. Будний день");
+
+if(number==2)
+Console.WriteLine("Вторник. Будний день");
+
+if(number==3)
+Console.WriteLine("Среда. Будний день");
+
+if(number==4)
+Console.WriteLine("Четверг. Будний день");
+
+if(number==5)
+Console.WriteLine("Пятница. Будний день");
+
+if(number==6)
+Console.WriteLine("Суббота. Будний день");
+
+if(number==7)
+Console.WriteLine("Воскресенье. Будний день");
