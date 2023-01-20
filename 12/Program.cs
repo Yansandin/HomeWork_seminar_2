04aba7d987ad1aa140a184a06f651db31fe6744a
San <yan_san_din@mail.ru>
2023-01-20 23:10:54 +0300

    first commit

---
+++ 12/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число- ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+string thirdDigit = Convert.ToString(number);
+
+if (thirdDigit.Length > 2)
+{
+  Console.WriteLine("Третья цифра -  " + thirdDigit[2]);
+}
+else 
+{
+  Console.WriteLine("Третьей цифры нет");
+}
